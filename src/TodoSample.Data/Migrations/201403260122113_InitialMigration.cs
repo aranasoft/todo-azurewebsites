392ae7f0b33b25a1b81491091d317363d14095ed
Dennis Burton <dennis.burton@gmail.com>
2014-03-28 18:00:00 -0400

    add missing migrations to data project and ef reference to api project

---
+++ src/TodoSample.Data/Migrations/201403260122113_InitialMigration.cs
@@ -0,0 +1,28 @@
+namespace TodoSample.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Todos",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Completed = c.Boolean(nullable: false),
+                        Content = c.String(),
+                        Points = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Todos");
+        }
+    }
+}
